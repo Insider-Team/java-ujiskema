44d8d07fd775d9c5016e5815285f461adc467c57
Raihan <sirradf@gmail.com>
2021-02-07 22:16:53 +0700

    Remove searching and Sorting

--- src/main/java/Person/Utama.java
+++ src/main/java/Person/Utama.java
@@ -6,14 +6,7 @@ public class Utama {
     private static String nama;
 
     public static void main(String[] args) {
-        
-        /*..::: HEADER :::..*/
-        System.out.println("!...:::::::::::::::::::::::::::::::::::::::::::::::::...!");
-        System.out.println("==================| SMKN 1 BONDOWOSO |===================");
-        System.out.println("===================| UJISKEMA PBO 2021 |====================");
-        System.out.println("!...::::::::::::::::::::::::::::::::::::::::::::::::::...!");
-        
-        /*..::: MENAMPILKAN ERROR OPSI MENU :::..*/
+
         try{
             pilihanMenu();
        }catch (Exception e){
@@ -30,63 +23,45 @@ public class Utama {
         System.out.println("");
         
         do{
-        //System.out.println("");
-        
-        /*..::: TAMPILAN OPSI MENU :::..*/
-        System.out.println("..:::||| PROGRAM UTAMA UJISKEMA2020 |||:::...");
+        System.out.println("===== PROGRAM UTAMA =====");
         System.out.println("1. Masukkan Data Guru");
         System.out.println("2. Masukkan Data Nilai Siswa");
         System.out.println("3. Tampilkan Data Guru");
         System.out.println("4. Tampilkan Data Nilai");
-        System.out.println("5. Pengurutan Data Siswa");
-        System.out.println("6. Pencarian Data Siswa");
-        System.out.println("7. Keluar Program");
-        System.out.println(".........:::::::::::::::::::::::::::::::.........");
+        System.out.println("5. Keluar Program");
+        System.out.println("=========================");
         System.out.println();
             
             Scanner pilihKodeMenu = new Scanner(System.in);
             System.out.print("Masukan Kode Opsi Menu : ");
             int opsiMenu = pilihKodeMenu.nextInt();
 
-            /*..::: PROSES MENAMPILKAN INPUTAN DARI OPSI MENU :::..*/
             switch(opsiMenu){
                 case 1 :
                     //Set data guru
-                    objGuru0.setDataGuru("John Roger",20185277,"Bahasa Jepang","\tXII");
-                    objGuru1.setDataGuru("Rempal Smith",20182003,"Olahraga","\tXII");
-                    objGuru2.setDataGuru("Reza Ranco",20181999,"Perbankan","\tXII");
+                    objGuru0.setDataGuru("Gayudh",20185269,"Bahasa Jepang","\tXII");
+                    objGuru1.setDataGuru("Albert Smith",20182420,"Olahraga","\tXII");
+                    objGuru2.setDataGuru("Asyuw Ranco",20181999,"Perbankan","\tXII");
 
                     System.out.println("Data berhasil dimasukkan dengan cara set!");
                     break;
                 case 2:
-                    /*..::: INPUT DATA NILAI SISWA :::..*/       
                     objSiswa.inputDataNilaiSiswa();
                     System.out.println("DATA BERHASIL DIMASUKKAN"); 
                     break;
                 case 3 :
                     System.out.println("NAMA\t\tNIP\t\tMAPEL\t\t\tKELAS");
-                    /*..::: VIEW DATA GURU :::..*/
                     objGuru0.infoPerson();
                     objGuru1.infoPerson();
                     objGuru2.infoPerson();
                     System.out.println("");
                     break;
                 case 4:
-                    /*..::: VIEW DATA NILAI SISWA :::..*/
                     System.out.println("");
                     objSiswa.infoPerson();
                     System.out.println("");
                     break;
                 case 5:
-                    /*..::: SORTING DATA NILAI :::..*/
-                    objSiswa.sortingDataNilaiSiswa();
-                    break;
-                case 6:
-                    /*..::: SEARCHING DATA NILAI BERDASAR NAMA :::..*/
-                    objSiswa.searching();
-                    break;
-                case 7:
-                    /*..::: EXIT :::..*/
                     System.exit(0);
                     break;
             }
